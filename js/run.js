c76eafb94e8558fc5a1e1ef32254e69d7fa46a23
aixiaocainiao <18338451979@163.com>
2023-12-16 21:32:37 +0800

    Site updated: 2023-12-16 21:32:35

--- js/run.js
+++ js/run.js
@@ -19,8 +19,8 @@ function createtime() {
   let c = "";
   (c =
     s < 18 && s >= 9
-      ? `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='浑水摸鱼罢了~'><br> <div style="font-size:13px;font-weight:bold">本站运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 继续陪伴 相爱相杀 日常斗嘴 ${r+394} 天 🚀</div>`
-      : `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title='终于下班啦~'><br> <div style="font-size:13px;font-weight:bold">本站运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 继续陪伴 相爱相杀 日常斗嘴 ${r+394} 天 🚀</div>`),
+      ? `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='浑水摸鱼罢了~'><br> <div style="font-size:13px;font-weight:bold">本站运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 下辈子见 ${r-394} 天 🚀</div>`
+      : `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title='终于下班啦~'><br> <div style="font-size:13px;font-weight:bold">本站运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 下辈子见 ${r-394} 天 🚀</div>`),
     document.getElementById("workboard") &&
       (document.getElementById("workboard").innerHTML = c);
 }
